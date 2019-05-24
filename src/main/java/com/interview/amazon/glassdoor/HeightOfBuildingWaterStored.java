9c8b1f60f2132c885e6f99c12c8a89fc12425ed5
Wendler Zacariotto <wenzaca@gmail.com>
2019-05-24 08:19:48 +0100

    Adding new challenge solution to 9 ball weight and adding new 2 todo test

---
+++ src/main/java/com/interview/amazon/glassdoor/HeightOfBuildingWaterStored.java
@@ -0,0 +1,9 @@
+package com.interview.amazon.glassdoor;
+
+/*
+Given an array of integers representing heights of structures, calculate the amount of water that can be trapped in between them (algorithms)
+ */
+public class HeightOfBuildingWaterStored {
+
+    //TODO
+}
---
+++ src/main/java/com/interview/amazon/glassdoor/MergeSotedArraysToSortedArray.java
@@ -0,0 +1,9 @@
+package com.interview.amazon.glassdoor;
+
+/*
+Merge 2 sorted Arrays into 1 Sorted array
+ */
+public class MergeSotedArraysToSortedArray {
+
+    //TODO
+}
